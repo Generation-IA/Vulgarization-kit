151b7210ce6f9472cdbd0eee169129b9e6f30fdb
zac <zachary.gagnou@epfedu.fr>
2022-04-02 14:54:41 +0200

    Added buttons

--- kit/src/First.js
+++ kit/src/First.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import logo from './img/Logo_group.svg';
+import button1 from './img/button-1.png';
+import button2 from './img/button-2.png';
 import './css/First.css';
-
+import { Container, Col, Row, Button } from "react-bootstrap";
 class First extends React.Component {
     constructor(props) {
         super(props)
@@ -9,9 +11,19 @@ class First extends React.Component {
 
     render() {
         return (
-            <div class="center">
-                <img class="main" src={logo} alt="logo"></img>
-            </div>
+            <Container>
+                <Row>
+                    <img class="chbutton" src={button1}></img>
+                </Row>
+                <Row>
+                    <img class="chbutton" src={button2}></img>
+                </Row>
+                <Row>
+                    <div class="center">
+                        <img src={logo} alt="logo"></img>
+                    </div>
+                </Row>
+            </Container>
         )
     }
 }
