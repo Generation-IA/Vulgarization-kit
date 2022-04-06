062742a48c396d012d5d79a00ca1f3cae1082f8e
zac <zachary.gagnou@epfedu.fr>
2022-04-06 14:10:08 +0200

    Added beggin button

--- kit/src/First.js
+++ kit/src/First.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './img/Logo_group.svg';
 import button1 from './img/button-1.png';
 import button2 from './img/button-2.png';
+import button3 from './img/button-3.png';
 import './css/First.css';
 import { Container, Col, Row, Button } from "react-bootstrap";
 class First extends React.Component {
@@ -12,16 +13,24 @@ class First extends React.Component {
     render() {
         return (
             <Container>
+
                 <Row>
-                    <img class="chbutton" src={button1}></img>
+                    <Col>
+                    </Col>
+                    <Col sm={2}>
+                        <Row>
+                            <img className="chbutton" src={button1}></img>
+                        </Row>
+                        <Row>
+                            <img className="chbutton" src={button2}></img>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row class='logorow'>
+                        <img src={logo} alt="logo" className='logo'></img>
                 </Row>
                 <Row>
-                    <img class="chbutton" src={button2}></img>
-                </Row>
-                <Row>
-                    <div class="center">
-                        <img src={logo} alt="logo"></img>
-                    </div>
+                    <img className="decouv" src={button3}></img>
                 </Row>
             </Container>
         )
