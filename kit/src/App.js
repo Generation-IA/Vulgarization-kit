112eb3fe8be6dc73e79fb0fe17edb66d50a1c857
zac <zachary.gagnou@epfedu.fr>
2022-05-17 16:24:25 +0200

    added tinder card

--- kit/src/App.js
+++ kit/src/App.js
@@ -5,6 +5,7 @@ import Decouverte from "./Decouverte.js";
 import At1 from "./At1.js";
 import At2 from "./At2.js";
 import Explication1 from "./Explication1.tsx";
+import Tinder1 from "./Tinder1.js";
 import "bootstrap/dist/css/bootstrap.css";
 // import Col from 'react-bootstrap/Col';
 // import Row from 'react-bootstrap/Row';
@@ -70,6 +71,7 @@ class App extends React.Component {
         <Decouverte />
         <At1 />
         <Explication1 />
+        <Tinder1 />
         <At2 />
       </div>
     );
---
+++ kit/src/Tinder1.js
@@ -0,0 +1,145 @@
+import React, { useState, useMemo, useRef } from "react";
+import userEvent from "@testing-library/user-event";
+import TinderCard from "react-tinder-card";
+import "./css/Tinder1.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Tinder1() {
+  const questions = [
+    {
+      index: 3,
+      title: "Do you think robots will rule the world ?",
+      answers: ["Yes", "No"],
+    },
+    {
+      index: 2,
+      title: "Are you interested in AI ?",
+      answers: ["Yes", "No"],
+    },
+    {
+      index: 1,
+      title: "Have you ever heard about AI ?",
+      answers: ["Yes", "No"],
+    },
+  ];
+
+  const results = [];
+
+  const [currentIndex, setCurrentIndex] = useState(questions.length - 1);
+  const [lastDirection, setLastDirection] = useState();
+  // used for outOfFrame closure
+  const currentIndexRef = useRef(currentIndex);
+
+  const childRefs = useMemo(
+    () =>
+      Array(questions.length)
+        .fill(0)
+        .map((i) => React.createRef()),
+    []
+  );
+
+  const updateCurrentIndex = (val) => {
+    setCurrentIndex(val);
+    currentIndexRef.current = val;
+  };
+
+  const canGoBack = currentIndex < questions.length - 1;
+
+  const canSwipe = currentIndex >= 0;
+
+  const swiped = (direction, nameToDelete, index) => {
+    setLastDirection(direction);
+    updateCurrentIndex(index - 1);
+    console.log("You swiped: " + direction);
+    if (direction === "right") {
+      results.push("Yes");
+    } else {
+      results.push("No");
+    }
+    console.log("RESULTS " + results); // Swipe t
+  };
+
+  const outOfFrame = (name, idx) => {
+    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current);
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard();
+  };
+
+  const swipe = async (dir) => {
+    if (canSwipe && currentIndex < questions.length) {
+      await childRefs[currentIndex].current.swipe(dir);
+    }
+  };
+
+  // increase current index and show card
+  const goBack = async () => {
+    if (!canGoBack) return;
+    const newIndex = currentIndex + 1;
+    updateCurrentIndex(newIndex);
+    await childRefs[newIndex].current.restoreCard();
+  };
+
+  questions.map((item) => {
+    console.warn("Index ", item);
+  });
+
+  const onCardLeftScreen = (myIdentifier) => {
+    console.log(myIdentifier + " left the screen");
+  };
+
+  return (
+    <div className="App">
+      <p>QUIZZ TIME BITCH</p>
+
+      {questions.map((item, index) => (
+        <center>
+          <TinderCard
+            ref={childRefs[index]}
+            className="swipe"
+            key={item.name}
+            onSwipe={(dir) => swiped(dir, item.name, index)}
+            onCardLeftScreen={() => outOfFrame(item.name, index)}
+            preventSwipe={["up", "down"]}
+          >
+            <img src="cardLogo.svg" alt="" />
+            <div className="title">
+              Culture <br /> Générale
+            </div>
+            <h1>
+              {item.index} : {item.title} <br />
+              {/* <li class="answer">{item.answers[0]}</li>
+              <li class="answer">{item.answers[1]}</li> */}
+            </h1>
+            {/* <div className="buttons">
+              <button
+                type="button"
+                class="btn btn-danger"
+                style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+                onClick={() => swipe("left")}
+              >
+                Swipe left!
+              </button>
+              <button
+                type="button"
+                class="btn btn-primary"
+                style={{ backgroundColor: !canGoBack && "#c3c4d3" }}
+                onClick={() => goBack()}
+              >
+                Undo swipe!
+              </button>
+              <button
+                type="button"
+                class="btn btn-success"
+                style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+                onClick={() => swipe("right")}
+              >
+                Swipe right!
+              </button> */}
+            {/* </div>{" "} */}
+          </TinderCard>
+        </center>
+      ))}
+    </div>
+  );
+}
+
+export default Tinder1;
